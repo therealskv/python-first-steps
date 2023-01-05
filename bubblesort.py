69e9faf56355bf14f8e293b5d9b15a2e31311adf
Sathish Velayudam <therealskv79@gmail.com>
2023-01-04 19:52:47 -0500

    Bubble sort

---
+++ bubblesort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+    return arr
+
+arr = [100, 20, 15, 5, 25, 10, 50, -30, -40, 75]
+print(bubble_sort(arr))

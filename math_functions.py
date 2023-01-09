2b6620e9ef5bf485833cea89a65d5b82dbc684c4
Sathish Velayudam <therealskv79@gmail.com>
2023-01-08 23:10:51 -0500

    Math functions

---
+++ math_functions.py
@@ -0,0 +1,9 @@
+import math
+
+def math_functions(a, b):
+    print("isfinite =", math.isfinite(a))
+    print("log = ", math.log(a))
+    print("isclose = ", math.isclose(a, b))
+    print("pi =", math.pi)
+
+math_functions(10, 5)

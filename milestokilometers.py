29469bae29ccb1184b0015c387395ffbbaa3d7b6
Sathish Velayudam <therealskv79@gmail.com>
2023-02-16 23:56:44 -0500

    Miles to kilometers

---
+++ milestokilometers.py
@@ -0,0 +1,8 @@
+miles = float(input("Enter value in miles: "))
+
+#conversion factor
+conv_fac = 0.621371
+
+kilometers = miles / conv_fac
+
+print('%0.2f miles is equal to %0.2f kilometers' %(miles, kilometers))

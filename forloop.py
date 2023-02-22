fd5f0b12d8e79acd3d79038ae4a2c8ac9398cd49
Sathish Velayudam <therealskv79@gmail.com>
2023-02-21 23:59:31 -0500

    For loop

---
+++ forloop.py
@@ -0,0 +1,24 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#looping through a string
+for x in "banana":
+    print(x)
+
+#looping through a range
+for x in range(6):
+    print(x)
+
+#range with starting value
+for x in range(2, 6):
+    print(x)
+
+#range with increment
+for x in range(0, 10, 2):
+    print(x)
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
